ac49000118b88892d0aebc7b48ae9ea413434e33
Kamaludin <kamal.zyel@gmail.com>
2021-06-28 14:59:26 +0700

    tailwind purgecss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  purge: [],
+  purge: {
+    mode: 'layers',
+    content: [
+      './public/**/*.html',
+      './src/**/*.vue'
+    ]
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
